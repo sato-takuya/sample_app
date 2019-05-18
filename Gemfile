8cd97ebce7ef54c102e844e86e3805beedf7dff0
SATOTAKUYA <>
2019-05-19 00:38:12 +0900

    Add user microposts

--- Gemfile
+++ Gemfile
@@ -68,3 +68,6 @@ gem "jquery-rails"
 gem 'faker','1.7.3'
 gem 'will_paginate'
 gem 'bootstrap-will_paginate'
+gem 'carrierwave','1.2.2'
+gem 'mini_magick','4.7.0'
+gem 'fog', '1.42'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new
@@ -51,14 +52,7 @@ class UsersController < ApplicationController
     params.require(:user).permit(:name, :email, :password,:password_confirmation)
   end
 
-  # ログイン済みユーザーかどうか確認
-  def logged_in_user
-    unless logged_in?
-      store_location
-      flash[:danger] = "Please log in."
-      redirect_to login_url
-    end
-  end
+
 
   def current_user?(user)
     user == current_user
